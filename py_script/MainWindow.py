c11d574f0b9a3a6f863cd89ebdab3b74b4ed33ca
HYOJAE15 <tls15gywo@gmail.com>
2022-04-29 21:48:30 +0900

    4/29 final

--- py_script/MainWindow.py
+++ py_script/MainWindow.py
@@ -14,16 +14,21 @@ def resource_path(relative_path):
 
 # UI파일 연결, MainWindow 열고 각 menu 들은 Dialog 로 실행 
 # mainwind.ui 에서 각 메뉴의 Dialog창을 띄운다.
-# MainWindow ui
+
+# MainWindow
 form_main = resource_path("../ui_design/mainwind.ui")
 form_class_main = uic.loadUiType(form_main)[0]
+
 # ToolsDialog
 form_tools = resource_path("../ui_design/toolsdial.ui")
 form_class_tools = uic.loadUiType(form_tools)[0]
-# 
+
+# ImageDialog
+form_image = resource_path("../ui_design/imagedial.ui")
+form_class_image = uic.loadUiType(form_image)[0]
 
 
-# menu optiondialog
+# menu
 # ToolsDialog
 class ToolsDialog(QDialog, form_class_tools) :
     def __init__(self, parent) :
@@ -31,9 +36,29 @@ class ToolsDialog(QDialog, form_class_tools) :
         self.setupUi(self)
         self.show()
 
+# ImageDialog, when imagefile open
+# 이미지 사이즈 대로 출력한다.
+class ImageDialog(QDialog, form_class_image) :
+    def __init__(self, parent) :
+        super().__init__(parent)
+        self.setupUi(self)
+        self.show()
+
+        # ImageDialog : 내부 버튼 
+        self.btn_img_op.clicked.connect(self.btn_img_opFunction)
+    # ImageDialog 내부 버튼 클릭시  
+    # 사진 사이즈 대로 pixmap resize 하고 그에 맞게 label 이 조정 되면서 사진 크기 에 맞게 출력 된다
+    def btn_img_opFunction(self):
+        fname = QFileDialog.getOpenFileName(self, 'Open file','/', "Image files (*.jpg *.png)")
+        imagePath = fname[0]
+        pixmap = QPixmap(imagePath)
+        self.label.setPixmap(QPixmap(pixmap))
+        self.resize(pixmap.width(), pixmap.height())
+
 
 
 # Mainwindow
+
 class MainWindow(QMainWindow, form_class_main) :
     def __init__(self) :
         super().__init__()
@@ -41,12 +66,22 @@ class MainWindow(QMainWindow, form_class_main) :
         
         # menubar action
         self.actionTools.triggered.connect(self.actionToolsFunction)
+        self.actionOpen.triggered.connect(self.actionOpenFunction)
+        self.actionOpen_2.triggered.connect(self.actionOpen_2Function)
 
+    # menubar action Function
 
     def actionToolsFunction(self) :
         ToolsDialog(self)  
 
+    def actionOpenFunction(self) :
+        ImageDialog(self)
 
+
+    def actionOpen_2Function(self) :
+        filename = QFileDialog.getOpenFileName(self, 'open image', 'C:/', 'images (*.jpg *.psd *.png)')
+        imagepath = filename[0]
+        pixmap = QPixmap(imagepath)
 
 
 

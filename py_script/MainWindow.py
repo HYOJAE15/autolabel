face4c1154ead39064692c0a1e085482ccd1350b
HYOJAE15 <tls15gywo@gmail.com>
2022-05-13 17:40:56 +0900

    5/13 image blend

--- py_script/MainWindow.py
+++ py_script/MainWindow.py
@@ -1,27 +1,27 @@
 import os
 import sys
 
-from PyQt5 import QtCore, QtGui, uic
-from PyQt5.QtCore import QModelIndex, Qt, pyqtSlot
-from PyQt5.QtGui import QPixmap
+import cv2
+
+import numpy as np 
+
+from PyQt5 import QtCore, QtGui, uic, QtWidgets
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+from utils import resource_path
+from Canvasmodule import Canvas
 
 
-def resource_path(relative_path): 
-    """ Get absolute path to resource, works for dev and for PyInstaller """ 
-    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__))) 
-    return os.path.join(base_path, relative_path)
-# import os, import sys  한다음 위 함수 정의 해서 pyinstaller 오류 해결
 
-
-# UI파일 연결, MainWindow 열고 각 menu 들은 Dialog 로 실행 
-# mainwind.ui 에서 각 메뉴의 Dialog창을 띄운다.
 
 # MainWindow UI
-form_main = resource_path("../ui_design/mainwind.ui")
-form_class_main = uic.loadUiType(form_main)[0]
+ui_folder = 'ui_design'
+project_folder_ui = 'mainWindow.ui'
 
-
+form = os.path.join(ui_folder, project_folder_ui)
+form_class_main = uic.loadUiType(form)[0]
 
 
 # Mainwindow class
@@ -31,8 +31,6 @@ class MainWindow(QMainWindow, form_class_main) :
         super().__init__()
         self.setupUi(self)
 
-    
-        
         # treeview
         self.folderPath = None
         self.pathRoot = QtCore.QDir.rootPath()
@@ -41,31 +39,53 @@ class MainWindow(QMainWindow, form_class_main) :
         self.treeView.clicked.connect(self.treeViewImage)
 
 
+        # label opacity horizontalslider
+        self.win_name = "Label Opacity"
+        self.slide_name = "Opacity"
+    
+
+
         # file 메뉴 
         self.actionOpenImage.triggered.connect(self.actionOpenImageFunction)
         self.actionOpenFolder.triggered.connect(self.actionOpenFolderFunction)
 
         # zoom in and out
         self.scale = 1
+        self.alpha = 1
         self.zoomInButton.clicked.connect(self.on_zoom_in)
         self.zoomOutButton.clicked.connect(self.on_zoom_out)
 
-        # Brush
-        #self.brushButton.clicked.connect(self.burshFunction)
+        # Brush 사용이 가능하도록 scrollArea 에 canvas 클래스 설정
+        # brush 사용이 가능한 scrollArea 에 이미지를 불러 사용한다.
+        self.canvas = Canvas()
+        self.scrollArea.setWidget(self.canvas)
+
+        # image blending & label opacity
+        # image blending 을 통해 original, colored  이미지 2장을 불러와 blending 후 alpha 값 조절해서 투명도 조절
+        # self.img1 = 
+
+        # self.img2 = 
 
        
 
 
 
 
-    # menubar action Function
+        # menubar action Function
 
     # 메뉴바의 openimage 클릭시 mainimageviewer 에 이미지를 보여준다
     def actionOpenImageFunction(self) :
+        """To be depreciated.
+        """
         filename = QFileDialog.getOpenFileName(self, 'open image', 'C:/', 'images (*.jpg *.psd *.png)')
+        print(f'filename', filename)
         imagepath = filename[0]
-        pixmap = QPixmap(imagepath)
-        self.mainImageViewer.setPixmap(QPixmap(pixmap))
+        print(f'imagepath', imagepath)
+        cv_img1 = cv2.imread(imagepath)
+        print(f'cv_img1', cv_img1)
+
+        self.canvas.readImageFromFile(imagepath)
+        
         
 
     # 메뉴바 의 openfolder 클릭시 treeview 에 해당 폴더를 보여준다, treeview function
@@ -80,25 +100,47 @@ class MainWindow(QMainWindow, form_class_main) :
 
     # treeview 에서 파일 클릭 시 클릭된 파일 로드 
     # 이미지 파일 클릭시 묶여있는 라벨링 이미지 와 함께 불러와 합성후 하나의 파일로 만들어 보여준다
-    @pyqtSlot(QModelIndex)
+    # @pyqtSlot(QModelIndex)
     def treeViewImage(self, index) :
 
         indexItem = self.treeModel.index(index.row(), 0, index.parent())
-        filePath = self.treeModel.filePath(indexItem)
+        imgPath = self.treeModel.filePath(indexItem)
+        #cv_img1 = cv2.imread(filePath)
+        # open cv image blending 시 필요한 이미지 2장을 어떻게 엮나?? original 과 페어링 된 colored 이미지 불러오기
+        #cv_img2 = cv2.imread("./py_script/img_test_01.png")
+        # print(f'indexitem', indexItem)  # treeview 에서 파일 클릭시 파일의 인덱스(?)를 저장 한다
+        # print(f'imgPath', imgPath)  # 클릭한 파일의 인덱스 로 부터 해당 파일의 주소를 저장 한다.(해당 파일 주소 : 문자열)
+       
+        self.img = cv2.imread(imgPath) # 이미지 주소 내부 에 "사진" 을 인식하지 못한다, desktop 에서 test 실행
+        self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB) # 이미지 주소 내부 에 "사진" 을 인식하지 못한다, desktop 에서 test 실행
 
-        self.pixmap = QPixmap(filePath) 
-        # self.original_image = QPixmap(org_img_filePath) # original image path
-        # self.label_image = QPixmap(lab_img_filePath) # label image path
-        # self.pallette 
-        # self.color_image 
+        labelPath = imgPath.replace('/leftImg8bit/', '/gtFine/')
+        labelPath = labelPath.replace( '_leftImg8bit.png', '_gtFine_labelIds.png')
 
-        # blend 
-        # self.blendid_image = self.org_img * 0.7 + self.label * 0.3
+        self.label = cv2.imread(labelPath, cv2.IMREAD_UNCHANGED) 
 
-        self.scale = self.mainImageViewer.height() / self.pixmap.height()
-        self.resize_image()
+        self.color_label = np.zeros_like(self.img)
 
+        alpha = 0.8
 
+        # background, crack, rebar exposure, spalling, efflorescence 
+        pallete = np.array([[0, 0, 0], [255, 0, 0], [0, 255, 0], [0, 0, 255], [0, 255, 255]])
+
+        for idx, color in enumerate(pallete) : 
+            self.color_label[self.label == idx, :] = self.img[self.label == idx, :] * alpha + color * (1-alpha)
+
+        
+        self.canvas.readImageFromArray(self.color_label)
+
+        # 이미지를 띄울 캔버스 에 맞게 스케일 조절
+        self.ver_scale = self.canvas.image.height() / self.scrollArea.height()
+        self.hzn_scale = self.canvas.image.width() / self.scrollArea.width()
+
+        self.canvas.setVerticalScale(self.ver_scale)
+        self.canvas.setHorizontalScale(self.hzn_scale)
+        
+
+    # canvas 클래스로 설정된 scrollarea 에 띄워진 이미지를 줌 하는 기능 구현 
     def on_zoom_in(self, event):
         self.scale *= 2
         self.resize_image()
@@ -109,15 +151,20 @@ class MainWindow(QMainWindow, form_class_main) :
 
     def resize_image(self):
         
-        size = self.pixmap.size()
-        scaled_pixmap = self.pixmap.scaled(self.scale * size)
-        self.mainImageViewer.setPixmap(scaled_pixmap)
-    
-    
+        size = self.qimage.size()
+        scaled_pixmap = self.qimage.scaled(self.scale * size)
+        self.mainImageViewer.setPixmap(QPixmap.fromImage(scaled_pixmap))
+
+
     # 투명도 조절 함수 
     # def change_opacity 
         # op_change_image = TWE$R^#$@%^
         # self.mainImageViewer.setPixmap(op_change_image)
+
+    def onChange(self, x):
+        alpha = x/100
+        dst = cv2.addWeighted(self.cv_img1, 1-alpha, self.cv_img2, alpha, 0) 
+        cv2.imshow(self.win_name, dst)
 
 
 
